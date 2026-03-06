079c46acac03ad4ac8d149fa2d665dbc6487c591
Saif-Mansour-50 <saif200716@gmail.com>
2026-03-07 01:27:20 +0200

    notification-page

---
+++ src/app/core/services/notifications/notifications.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationsService {
+  private readonly httpClient = inject(HttpClient);
+
+  getNotifications(): Observable<any> {
+    return this.httpClient.get(environment.baseUrl + '/notifications?unread=false&page=1&limit=10');
+  }
+}
--- src/app/features/notifications/notifications.component.ts
+++ src/app/features/notifications/notifications.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { NotificationsService } from '../../core/services/notifications/notifications.service';
+import { Notification } from '../../core/models/notification/notification.interface';
 
 @Component({
   selector: 'app-notifications',
@@ -6,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.css',
 })
-export class NotificationsComponent {
+export class NotificationsComponent implements OnInit {
+  private readonly notificationsService = inject(NotificationsService);
 
+  notList: Notification[] = [];
+
+  ngOnInit(): void {
+    this.notificationsService.getNotifications().subscribe({
+      next: (res) => {
+        this.notList = res.data.notifications;
+        console.log('notification', this.notList);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
